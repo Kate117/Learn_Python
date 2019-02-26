7386a35e5e9c7d8046eae313ae4b521459c985c4
Liu Guifeng <liuguifeng@MacBook-Air.lan>
2019-02-26 21:13:41 +0800

    Second version of Lucky_Draw project

--- lucky_draw.py
+++ lucky_draw.py
@@ -38,7 +38,8 @@ Date: 2/22/2019 ~ 2/28/2019
 
 import random
 
-
+n=13
+m=37
 
 def lucky_draw(name_list=None):
     """
@@ -50,23 +51,32 @@ def lucky_draw(name_list=None):
 
     round_1 = input('Are you ready to select the 3rd prize?(Y/N) \n>')
     answer = ('Y','y')
+    print('The lucky ones for the 3rd prize are: \n' + '='*n + 'Third Prize' + '='*n)
     if round_1 in answer:
         name1 = random.sample(name_list, 3)
-        print(name1)
+        for name in name1:
+            print(' '*n+ name)
         for name in name1:
             name_list.remove(name)
-        print('Now there are remaining lucky ones:', name_list)
+    print("="*m)
 
     round_2 = input ('Ready for the 2nd prize?(Y/N) \n>')
+    print('The lucky ones for the 2nd prize are: \n' + '='*n + 'Second Prize' + '='*n)
     if round_2 in answer:
         name2 = random.sample(name_list, 2)
-        print(name2)
+        for name in name2:
+            print(' '*n + name)
         for r_name in name2:
             name_list.remove(r_name)
+    print('='*m)
+
     round_3 = input ('Now is the most exciting time, 1st prize! Ready?(Y/N) \n>')
+    print('The most lucky one for the 1st prize is: \n' + '='*n + 'First Prize' + '='*n)
     if round_3 in answer:
         name3 = random.sample(name_list, 1)
-        print(name3)
+        for name in name3:
+            print(' '*n + name)
+    print('='*m + '\n> End')
 
 
 def main():
@@ -75,3 +85,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
