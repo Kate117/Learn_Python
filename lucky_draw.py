6a7513e58e26ec88b7d354c4dc2c0247d4eb9d78
Liu Guifeng <liuguifeng@MacBook-Air.lan>
2019-03-05 22:13:30 +0800

    Third version of Lucky_Draw, added a while loop

--- lucky_draw.py
+++ lucky_draw.py
@@ -26,61 +26,75 @@ Step1: implement lucky_draw function.
         ========== First prize =========
                         F
         ================================
-    >> End
-
-Step2: increase yourself possibility of luck :)
-Step3: TODO
+    >> Once again? (Y/N): N
+    >> if N: Exit
+    >> if Y: Ask those questions above again.
+Step2: implement a loop running, and an option to exit after one turn.
+Step3: increase yourself possibility of luck :)
+Step4: TODO
 
 Author: Liu, Kate
 
 Date: 2/22/2019 ~ 2/28/2019
 """
-
+import time
 import random
+random.seed(time.time())
 
 n=13
 m=37
 
+
 def lucky_draw(name_list=None):
-    """
-    this is your code area
-    :param name_list:
-    :return:
-    """
-    print('Participants of the lucky draw are:',name_list)
 
-    round_1 = input('Are you ready to select the 3rd prize?(Y/N) \n>')
-    answer = ('Y','y')
-    print('The lucky ones for the 3rd prize are: \n' + '='*n + 'Third Prize' + '='*n)
-    if round_1 in answer:
-        name1 = random.sample(name_list, 3)
-        for name in name1:
-            print(' '*n+ name)
-        for name in name1:
-            name_list.remove(name)
-    print("="*m)
 
-    round_2 = input ('Ready for the 2nd prize?(Y/N) \n>')
-    print('The lucky ones for the 2nd prize are: \n' + '='*n + 'Second Prize' + '='*n)
-    if round_2 in answer:
-        name2 = random.sample(name_list, 2)
-        for name in name2:
-            print(' '*n + name)
-        for r_name in name2:
-            name_list.remove(r_name)
-    print('='*m)
 
-    round_3 = input ('Now is the most exciting time, 1st prize! Ready?(Y/N) \n>')
-    print('The most lucky one for the 1st prize is: \n' + '='*n + 'First Prize' + '='*n)
-    if round_3 in answer:
-        name3 = random.sample(name_list, 1)
-        for name in name3:
-            print(' '*n + name)
-    print('='*m + '\n> End')
+    your_words = 'Y'
+
+    while True:
+        name_list_tmp = name_list.copy()
+        if your_words in 'N':
+            print('=' * m + '\n> End')
+            break
+
+        if your_words in 'Y':
+            print('Participants of the lucky draw are:', name_list_tmp)
+            round_1 = input('Are you ready to select the 3rd prize?(Y/N) \n>')
+            answer = ('Y','y')
+            print('The lucky ones for the 3rd prize are: \n' + '='*n + 'Third Prize' + '='*n)
+            if round_1 in answer:
+                name1 = random.sample(name_list_tmp, 3)
+                for name in name1:
+                    print(' '*n+ name)
+                for name in name1:
+                    name_list_tmp.remove(name)
+            print("="*m)
+
+            round_2 = input ('Ready for the 2nd prize?(Y/N) \n>')
+            print('The lucky ones for the 2nd prize are: \n' + '='*n + 'Second Prize' + '='*n)
+            if round_2 in answer:
+                name2 = random.sample(name_list_tmp, 2)
+                for name in name2:
+                    print(' '*n + name)
+                for r_name in name2:
+                    name_list_tmp.remove(r_name)
+            print('='*m)
+
+            round_3 = input ('Now is the most exciting time, 1st prize! Ready?(Y/N) \n>')
+            print('The most lucky one for the 1st prize is: \n' + '='*n + 'First Prize' + '='*n)
+            if round_3 in answer:
+                name3 = random.sample(name_list, 1)
+                for name in name3:
+                    print(' '*n + name)
+        your_words = input('Once again? Y/N \n>')
+
+
+
+
 
 
 def main():
-    name_list = ['Anna', 'kate', 'Liangchun', 'Tina', 'tim', 'Turen','Shoujiang','Sarah','Tingting','Jingjing']
+    name_list = ['Anna', 'Kate', 'Liangchun', 'Tina', 'Tim', 'Turen','Shoujiang','Sarah','Tingting','Jingjing']
     lucky_draw(name_list)
 
 if __name__ == '__main__':
